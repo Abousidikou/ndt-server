9ebcb7a7894623fcd506f2f320f445099b8aadd0
Abousidikou <41793033+Abousidikou@users.noreply.github.com>
2022-01-27 15:03:27 +0100

    Create cpCerts.sh

---
+++ cpCerts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# 1 : username
+# 2 : cert and privkey path
+# 3 : ndt-server/certs path
+cp $2/fullchain.pem $3/
+cp $2/privkey.pem $3
+
+chown $1:$1 $3/fullchain.pem
+chown $1:$1 $3/privkey.pem
